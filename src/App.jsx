2521fcfe422f47373f92e19465e274ed63474d14
franklin-raph <igboekwulusifranklin@gmail.com>
2023-09-15 06:17:18 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Signin from './authentication/signin/Signin'
+import Signup from './authentication/signup/Signup'
+import Dashboard from './pages/dashboard/Dashboard'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <HashRouter>
+        <Routes >
+          <Route path='/' element={<Signin />}/>
+          <Route path='/signin' element={<Signin />}/>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+    </HashRouter>
   )
 }
 
--- src/authentication/signin/Signin.jsx
+++ src/authentication/signin/Signin.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Signin = () => {
+    const [loading, setLoading] = useState(false)
+    const [adminLevel, setAdminLevel] = useState(1)
+    const [adminEmail, setAdminEmail] = useState("")
+    const [adminPhoneNum, setAdminPhoneNum] = useState("")
+    const [adminPassWord, setAdminPassword] = useState("")
+    const [inputType, setInputType] = useState("password");
+    const [showPassword, setShowPassword] = useState(false)
+
+    async function handleAdminRegister(){
+
+    }
+
+    const toggleInput = () => {
+        setInputType(inputType === "password" ? "text" : "password");
+        setShowPassword(!showPassword);
+      };
+
   return (
-    <div>Signin</div>
+    <div class="bg-gradient-primary" style={{ height:"100vh", display:"flex", justifyContent:"center", alignItems:"center" }}>
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-xl-10 col-lg-12 col-md-9">
+
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <div class="row">
+                                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        </div>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user"
+                                                    id="exampleInputEmail" aria-describedby="emailHelp"
+                                                    placeholder="Enter Email Address..." />
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user"
+                                                    id="exampleInputPassword" placeholder="Password" />
+                                            </div>
+                                            <Link to="/dashboard" class="btn btn-primary btn-user btn-block">
+                                                Login
+                                            </Link>
+                                        </form>
+                                        <hr />
+                                        <div class="text-center">
+                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        </div>
+                                        <div class="text-center">
+                                          <Link class="small" to="/signup">Create an Account!</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
 
